86a098900ec806844152f05aee74a9750830f132
Zkaaaaaaa <azka.muzaffar@gmail.com>
2025-04-20 08:06:42 +0700

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,7 +10,7 @@
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                     @elseif (auth()->user()->role == 'employee')
-                    <a href="{{ route('employee.dashboard') }}"></a>
+                    <a href="{{ route('employee.dashboard') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                     @else
