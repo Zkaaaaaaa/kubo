4fa4063c7d62d133f3d9f521a07551444f544b87
Zkaaaaaaa <azka.muzaffar@gmail.com>
2025-06-28 00:37:40 +0700

    Update footer.blade.php

--- resources/views/layouts/admin/footer.blade.php
+++ resources/views/layouts/admin/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2001 <a href="http://adminlte.io">Azka Muzhaffar</a>.</strong>
+    <strong>Copyright &copy; 2025 <a href="http://adminlte.io">KUBO KOPI</a>.</strong>
     All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
+    {{-- <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> terbaiknya
-    </div>
+    </div> --}}
   </footer>
