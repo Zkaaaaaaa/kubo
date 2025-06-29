c59dcc8b6dae407cd3e94e06476c91904121eecf
Zkaaaaaaa <azka.muzaffar@gmail.com>
2025-06-29 16:29:07 +0700

    Update detail-product.blade.php

--- resources/views/client/detail-product.blade.php
+++ resources/views/client/detail-product.blade.php
@@ -25,8 +25,8 @@
                                 <input type="number" class="form-control" id="quantity" name="quantity" value="1" min="1" max="10" required onchange="validateQuantity(this)">
                             </div>
                             <div class="form-group">
-                                <label for="notes">Catatan</label>
-                                <textarea class="form-control" id="notes" name="notes" rows="3" placeholder="Tambahkan catatan untuk pesanan Anda (opsional)"></textarea>
+                                <label for="note">Catatan</label>
+                                <textarea class="form-control" id="note" name="note" rows="3" placeholder="Tambahkan catatan untuk pesanan Anda (opsional)"></textarea>
                             </div>
                             <button type="submit" class="btn btn-warning">Tambah ke Pesanan</button>
                         </form>
