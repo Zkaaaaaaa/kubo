24eb08be7cbaced21a486055322c6cb27bb184bd
Zkaaaaaaa <azka.muzaffar@gmail.com>
2025-06-29 15:16:29 +0700

    Update EmailVerificationPromptController.php

--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -14,6 +14,8 @@ class EmailVerificationPromptController extends Controller
      */
     public function __invoke(Request $request): RedirectResponse|View
     {
-        return view('auth.verify-email');
+        return $request->user()->hasVerifiedEmail()
+                    ? redirect()->intended(route('home', absolute: false))
+                    : view('auth.verify-email');
     }
 }
